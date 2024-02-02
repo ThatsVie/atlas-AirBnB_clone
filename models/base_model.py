27183575ac794e102897a488b7b494d02bfcfa83
ThatsVie <Viepaulalmt@gmail.com>
2024-02-02 14:19:12 -0800

    updating base

--- models/base_model.py
+++ models/base_model.py
@@ -4,13 +4,14 @@ Base Model Class Module for AirBnB project
 """
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
     """
     BaseModel class
     """
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         """
         Constructor method for BaseModel.
 
@@ -19,23 +20,31 @@ class BaseModel:
         created_at (datetime): Creation timestamp.
         updated_at (datetime): Last update timestamp.
 
-        Args:
-        **kwargs: Dictionary representation of the instance.
+        Parameters:
+        * args: variable-length list of arguments
+        **kwargs: variable-length dictionary of keyword arguments.
         """
+        # If instance is being created from a dictionary representation
         if kwargs:
             # If kwargs is not empty, update instance attributes
             for key, value in kwargs.items():
-                if key != '__class__':
-                    if key in ['created_at', 'updated_at']:
-                        value = datetime.strptime(
-                            value, '%Y-%m-%dT%H:%M:%S.%f'
-                        )
+                # Convert string representation of datetime to datetime object
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                # Exclude __class__ from setting as an attribute
+                if key != "__class__":
                     setattr(self, key, value)
+
+            # If id is not provided in kwargs, generate a new unique identifier
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+                self.created_at = self.updated_at = datetime.now()
         else:
-            # If kwargs is empty, create a new instance
+            # If instance is being created without a dictionary representation
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.created_at = self.updated_at = datetime.now()
+            # Register the new instance with the storage system
+            storage.new(self)
 
     def __str__(self):
         """
@@ -52,9 +61,10 @@ class BaseModel:
 
     def save(self):
         """
-        Update updated_at with the current datetime.
+        Saves instance and updates updated_at attribute to current datetime.
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
