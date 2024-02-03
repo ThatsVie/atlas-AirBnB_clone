080eb65b369fa6c352f18001b4a02fd1b77f24af
ThatsVie <Viepaulalmt@gmail.com>
2024-02-03 11:23:18 -0800

    amenity, place and review

--- models/review.py
+++ models/review.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""Review Class Module"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Review Class"""
+    place_id = ""
+    user_id = ""
+    text = ""
