e8c100f59256aa0c637c1dc276fe826479b803b7
Vie <143755961+ThatsVie@users.noreply.github.com>
2024-02-05 17:12:16 -0600

    Update test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -40,6 +40,20 @@ class TestFileStorage(unittest.TestCase):
         self.storage.reload()
         reloaded_keys = list(self.storage.all().keys())
         self.assertEqual(reloaded_keys, initial_keys)
+
+    def test_file_path_attribute(self):
+        self.assertEqual(FileStorage._FileStorage__file_path, "file.json")
+
+    def test_objects_attribute(self):
+        self.assertIsInstance(FileStorage._FileStorage__objects, dict)
+
+    def test_reload_method_with_existing_file(self):
+        initial_keys = list(self.storage.all().keys())
+        # Remove the existing file to simulate a fresh start
+        os.remove("file.json")
+        self.storage.reload()
+        reloaded_keys = list(self.storage.all().keys())
+        self.assertEqual(reloaded_keys, initial_keys)
         
 if __name__ == '__main__':
     unittest.main()
