00a5ab88ba401747d1aba280a44f0db3fb58601e
ajchapman777 <ajchapman777@gmail.com>
2024-02-06 14:18:31 -0800

    the rest of place.py

--- models/place.py
+++ models/place.py
@@ -19,3 +19,15 @@ class Place(BaseModel):
         longitude (float): The longitude of the place.
         amenity_ids (list): A list of Amenity ids.
     """
+
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
